c54f33dc534fd2a8ec67492b8d04bc31d8cfa15b
V. Lai <lai.vivian@gmail.com>
2018-02-21 15:48:21 -0800

    update socketiochallenge

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ const io = sio(server);
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('hello');
-});
+// app.get('/', (req, res) => {
+//   res.send('hello');
+// });
 
 const users = {};
 const messages = [];
@@ -21,12 +21,28 @@ const messages = [];
 // when a new connection is received
 io.on('connection', (socket) => {
   let userId = socket.id;
+
   // step 1. Listen to event where user emits her name, update users object
-  // step 2. After updating users object, broadcast event to update everyone else with new user list
-  // step 3. After updating users object, emit to new user with userList and message list
+  socket.on('name', function (name) {
+    users[userId] = name;
+    // step 2. After updating users object, broadcast event to update everyone else with new user list
+    socket.broadcast.emit('users', users);
+    // step 3. After updating users object, emit to new user with userList and message list
+    socket.emit('data', { users, messages });
+  });
+
   // step 4. Listen to event when user emits new message. Broadcast to everyone else with new message
+  socket.on('message', ({message}) => {
+    socket.broadcast.emit('message', {userId, message});
+    messages.push({userId, message});
+  });
+
   // step 5. Listen to event when user disconnects. Delete user and Broadcast new user list to everyone
+  socket.on('disconnect', () => {
+    delete users[userId];
+    io.emit('users', users);
+  })
 });
 
 // replace with your server port
-server.listen(3613);
+server.listen(3613, () => console.log('serving on port 3613'));
--- public/fe.js
+++ public/fe.js
@@ -5,24 +5,38 @@ let name = prompt('what is your username?');
 
 socket.on('connect', () => {
   // 2. on connect, emit your name to your server
+  socket.emit('name', { name });
 });
 
 let users = {};
 
 // 3. Bind to initial data to get all users and messages
 // call renderUsers(users), and renderMessages(messages) to load UI
+socket.on('data', (data) => {
+  users = data.users;
+  renderUsers(data.users);
+  renderMessages(data.messages);
+});
 
 // 4. Bind to event (when new user joins), then renderUsers(users);
+socket.on('users', (data) => {
+  users = data;
+  renderUsers(data);
+});
 
 // 5. Bind to event (when new message is received ), then call createMessage({name, message});
+socket.on('message', ({userId, message}) => {
+  createMessage({ name: users[userId].name, message });
+});
 
 const submit = (v) => {
   if(!name) return;
   // 6. submit is called when user hits enter. Emit an event to server with the message
+  socket.emit('message', {message: v});
   createMessage({name, message: v});
 };
 
-/* 
+/*
  * HELPER functions for rendering UI below
 */
 const chatContainer = document.getElementsByClassName('chatContainer')[0];
